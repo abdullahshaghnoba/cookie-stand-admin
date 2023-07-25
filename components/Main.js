f714a1fa3a60f4ee7ba566a2cf3e153e666512aa
abdullah <abdullahshagnoba@gmail.com>
2023-07-25 21:37:00 +0300

    feat: link front with back

--- components/Main.js
+++ components/Main.js
@@ -1,49 +1,54 @@
-import { useState } from "react";
-import Placeholder from "./Placeholder";
+export default function Main({ form, setForm, setFlag }) {
 
-export default function Main() {
-    const [form, setForm] = useState({
-      location: "",
-      minimum: "",
-      maximum: "",
-      average: ""
-    })
-    const [flag, setFlag] = useState(false)
-  
-  
-    function lastCookieAddedHandler(event) {
-      event.preventDefault();
-      setFlag(true)
+
+
+
+  function lastCookieAddedHandler(event) {
+
+    event.preventDefault();
+    let obj = {
+      location: event.target.location.value,
+      minimum: event.target.minimum.value,
+      maximum: event.target.maximum.value,
+      average: event.target.average.value
     }
-  
-    return (
-      <main className="flex flex-col items-center justify-center h-auto">
-        <form className="flex flex-col items-center justify-center w-auto h-auto p-10 bg-green-300" onSubmit={lastCookieAddedHandler} >
-          <h1 className="p-5 text-4xl font-bold">Create Cookie Stand</h1>
-          <div>
-            <div className="flex justify-around">
-              <label className="text-2xl">Location</label>
-              <input className="w-11/12 mx-auto" type="text" value={form.location} name="location" onChange={(event) => setForm({ ...form, location: event.target.value })}></input>
+    setForm([...form, obj])
+
+  }
+
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <form className="flex flex-col items-center justify-center p-10 bg-green-300" onSubmit={lastCookieAddedHandler} >
+        <h1 className="p-5 text-4xl font-bold">Create Cookie Stand</h1>
+        <div>
+          <div className="flex justify-around">
+            <div className="flex flex-col w-full p-5">
+              <label className="flex justify-center text-2xl">ADD Location</label>
+              <input className="w-full" type="text" name="location" Placeholder={'Cookie Stand Location'} ></input>
             </div>
-            <div className="flex p-5">
-              <div className="flex flex-col p-5">
-                <label className="text-2xl">Minimum Customers per Hour</label>
-                <input type="number" value={form.minimum} name="minimum" onChange={(event) => setForm({ ...form, minimum: event.target.value })}></input>
-              </div>
-              <div className="flex flex-col p-5">
-                <label className="text-2xl">Maximum Customers per Hour</label>
-                <input type="number" value={form.maximum} name="maximum" onChange={(event) => setForm({ ...form, maximum: event.target.value })}></input>
-              </div>
-              <div className="flex flex-col p-5">
-                <label className="text-2xl">Average Cookies per Sale</label>
-                <input type="number" value={form.average} name="average" onChange={(event) => setForm({ ...form, average: event.target.value })}></input>
-              </div>
-              <button className="flex items-center justify-center p-5 text-2xl bg-green-500" type="submit">Create</button>
+
+            <div className="flex flex-col">
+              <button className="flex items-center justify-center p-5 text-2xl bg-green-500 " type="submit">Create Stand</button>
             </div>
           </div>
-        </form>
-        <Placeholder flag={flag} form={form} />
-        {/* <p className="p-10 text-2xl"> Report Table Coming Soon...</p> */}
-      </main>
-    )
-  }
+          <div className="flex p-5">
+            <div className="flex flex-col p-5">
+              <label className="text-2xl">Minimum Customers per Hour</label>
+              <input type="number" name="minimum" ></input>
+            </div>
+            <div className="flex flex-col p-5">
+              <label className="text-2xl">Maximum Customers per Hour</label>
+              <input type="number" name="maximum" ></input>
+            </div>
+            <div className="flex flex-col p-5">
+              <label className="text-2xl">Average Cookies per Sale</label>
+              <input type="number" name="average" ></input>
+            </div>
+
+          </div>
+        </div>
+      </form>
+
+    </main>
+  )
+}
--- components/ReportTable.js
+++ components/ReportTable.js
@@ -1,6 +1,11 @@
-import { hours, cities, Totals } from '../Data'
+import { hours, cities } from '../Data'
 
-export default function ReportTable() {
+export default function ReportTable({ form , setForm}) {
+    
+    function deleteHandler(i) {
+        let newform = form.filter((item,index)=> index !== i)
+        setForm(newform)
+    }
 
     return (
         <div className="flex items-center justify-center m-8">
@@ -14,20 +19,28 @@ export default function ReportTable() {
                         </tr>
                     </thead>
                     <tbody>
-
-                        {cities.map((item, index) => (<>
-                            <tr className={index % 2 != 0 ? 'bg-green-300' : 'bg-green-400'}>
-                                {item.map(element => (
-                                    <td className='border border-black '>{element}</td>
+                        {form.map((item, index) => (
+                            <tr className={index%2!=0?'bg-green-400 ':'bg-green-300 '}>
+                                <td className='p-4 font-bold border border-black'>
+                                   <p> {item.location} </p><button onClick={() =>deleteHandler(index)} ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+</svg>
+</button>
+                                    </td>
+                                {cities.map((item, index) => (
+                                    <td className='p-4 font-bold border border-black'>{item}</td>
                                 ))}
                             </tr>
-                        </>))}
+                        ))}
+
+
 
                     </tbody>
                     <tfoot>
                         <tr>
-                            {Totals.map(item => (
-                                <td className='p-4 font-bold bg-green-600 border border-black' >{item}</td>
+                            <td className='p-4 font-bold bg-green-600 border border-black'>Totals</td>
+                            {cities.map(item => (
+                                <td className='p-4 font-bold bg-green-600 border border-black' >{item * form.length}</td>
                             ))}
                         </tr>
                     </tfoot>
